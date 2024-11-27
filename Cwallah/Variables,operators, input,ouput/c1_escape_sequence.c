205054bd0ac8ad951c99dd0e3e2e9b43130b19fd
oishorjo47 <tausifuddin280@gmail.com>
2024-11-27 21:58:59 +0600

    Add files via upload

---
+++ Cwallah/Variables,operators, input,ouput/c1_escape_sequence.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    printf("Hello Oishorjo\n");
+    printf("\n");
+    printf("How are you?\n");
+    printf("\nI am fine\nThanks!");
+    printf("\nOh!\tthat's great!\n");
+
+    return 0;
+}
